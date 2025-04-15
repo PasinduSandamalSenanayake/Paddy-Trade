852568359887c24652b6128dd20b77919532fd69
PasinduSandamalSenanayake <pasindusandamalsenanayake@gmail.com>
2025-04-15 11:33:53 +0530

    Initial Commit

---
+++ Paddy Trade/Paddy_TradeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Paddy_TradeApp.swift
+//  Paddy Trade
+//
+//  Created by Sandamal on 2025-04-15.
+//
+
+import SwiftUI
+
+@main
+struct Paddy_TradeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
