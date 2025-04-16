5e1fb41ce3106ea1131ce61bfeacd3895a2899e3
PasinduSandamalSenanayake <pasindusandamalsenanayake@gmail.com>
2025-04-16 09:03:14 +0530

    Local authentication

--- Paddy Trade/ContentView.swift
+++ Paddy Trade/ContentView.swift
@@ -6,18 +6,63 @@
 //
 
 import SwiftUI
+import LocalAuthentication
 
 struct ContentView: View {
+    @State private var isAuthenticated = false
+    @State private var showLogin = false
+
+    var body: some View {
+        content
+    }
+
+    @ViewBuilder
+    private var content: some View {
+        if isAuthenticated {
+            HomeView()
+        } else if showLogin {
+            SignInView(loginAction: {_ in 
+                
+            })
+        } else {
+            SplashView()
+                .onAppear(perform: authenticateUser)
+        }
+    }
+
+    func authenticateUser() {
+        let context = LAContext()
+        var error: NSError?
+
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            let reason = "Authenticate to access Paddy Trade"
+
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, _ in
+                DispatchQueue.main.async {
+                    if success {
+                        isAuthenticated = true
+                    } else {
+                        showLogin = true
+                    }
+                }
+            }
+        } else {
+            DispatchQueue.main.async {
+                showLogin = true
+            }
+        }
+    }
+}
+
+struct SplashView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            ProgressView("Checking Authentication...")
         }
         .padding()
     }
 }
+
 
 #Preview {
     ContentView()
